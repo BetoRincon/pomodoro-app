0906b29aec0e0ca0908d549bbe49d90d253f4a22
Alberto Rincon <alberto@resmarksystems.com>
2022-06-24 13:33:23 -0500

    implement taskCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Timer from './Timer/Timer.js'
+import TaskCard from'./TaskCard/TaskCard.js'
 import React, { useState } from 'react';
 import * as Constants from "./constants/constants";
 
@@ -7,9 +8,17 @@ import * as Constants from "./constants/constants";
 function App() {
   const [taskNumber, setTaskNumber] = useState(1);
   const [timeLabel, setTaskLabel] = useState("Time to focus!");
+  const [showTimeCard, toggleTimeCard] = useState(false);
 
   function updateTimeLabel(option){
     setTaskLabel(option.label)
+  }
+
+  function updateTaskLabel(){
+    if(!showTimeCard) {
+      setTaskNumber(taskNumber + 1);
+      toggleTimeCard(true);
+    }
   }
 
   return (
@@ -24,10 +33,11 @@ function App() {
       <div className='centered-div task-menu'>
         <h3> Tasks </h3>
       </div>
-      <div className='centered-div add-task-button'>
+      <div className='centered-div add-task-button' onClick={updateTaskLabel}>
         <img src="plus-circle-white.png"></img>
         <p>Add Task</p>
       </div>
+      <TaskCard showCard={showTimeCard}/>
     </div>
   );
 }
